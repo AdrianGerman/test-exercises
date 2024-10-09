3a75ff75239c27f7cba77f533705c3a878f5693c
AdrianGerman <adrian.german1019@gmail.com>
2024-10-09 14:26:31 -0600

    initial config

--- list-items-react/src/App.tsx
+++ list-items-react/src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <aside>
+        <h1>Prueba de React</h1>
+        <h2>Añadir y eliminar elementos de una lista</h2>
+        <form action="">
+          <label htmlFor="">
+            Elemento a introducir:{"   "}
+            <input
+              type="text"
+              name="item"
+              required
+              placeholder="Practicar ingles..."
+            />
+          </label>
+          <button>Añadir elemento a la lista</button>
+        </form>
+      </aside>
+
+      <section>
+        <h2>Lista de elementos</h2>
+        <ul>
+          <li>Videojuegos</li>
+          <li>Libros</li>
+          <li>Películas</li>
+          <li>Series</li>
+          <li>Dibujar</li>
+        </ul>
+      </section>
     </>
   )
 }
