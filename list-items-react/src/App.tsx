6151489d00f039bc3c4a02dccd3d7762a64943e5
AdrianGerman <adrian.german1019@gmail.com>
2024-10-11 15:24:01 -0600

    useSEO

--- list-items-react/src/App.tsx
+++ list-items-react/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import { Item } from "./components/Item"
 import { useItems } from "./hooks/useItems"
+import { useSEO } from "./hooks/useSEO"
 
 export type ItemId = `${string}-${string}-${string}-${string}-${string}`
 
@@ -12,6 +13,11 @@ export interface Item {
 
 function App() {
   const { items, addItem, removeItem } = useItems()
+
+  useSEO({
+    title: `[${items.length}] Lista de items`,
+    description: "Añadir y eliminar elementos de una lista"
+  })
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
