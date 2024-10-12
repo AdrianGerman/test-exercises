2ca537b9e54749d4f22f1b4e31e5f8f892a00685
AdrianGerman <adrian.german1019@gmail.com>
2024-10-12 08:03:27 -0600

    some styles

--- list-items-react/src/components/Item.tsx
+++ list-items-react/src/components/Item.tsx
@@ -1,3 +1,5 @@
+import "./Item.css"
+
 export function Item({
   text,
   handleClick
@@ -6,9 +8,27 @@ export function Item({
   handleClick: () => void
 }) {
   return (
-    <li>
+    <li className="list-items">
       {text}
-      <button onClick={handleClick}>Eliminar elemento</button>
+      <button onClick={handleClick}>
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M4 7l16 0" />
+          <path d="M10 11l0 6" />
+          <path d="M14 11l0 6" />
+          <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+          <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+        </svg>
+      </button>
     </li>
   )
 }
